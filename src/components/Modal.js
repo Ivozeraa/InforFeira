78c842c049c7b8dc8f049bd43fc8cdcdf536434c
Erik3331 <erikramon3331@gmail.com>
2024-11-13 12:00:30 -0300

    corno d++

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,47 +1,31 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import styles from '../css/Modal.module.css';
 
-// ESSA PORCARIA NAO FECHA
-
 function CustomModal({ isOpen, onRequestClose, index, onComplete }) {
   const [answer, setAnswer] = useState('');
-  const closeButtonRef = useRef(null); // Ref para o botão de fechar
 
   // Função para quando o formulário for enviado
   const handleAnswerSubmit = (e) => {
     e.preventDefault();
-    onComplete(); // Avisa que a resposta foi dada
-    closeModal(); // Fecha o modal manualmente
-  };
-
-  // Função para fechar o modal manualmente
-  const closeModal = () => {
-    // Verifica se a função de fechamento foi passada como prop, se sim, chama ela
-    if (onRequestClose) {
-      onRequestClose();
-    }
+    onComplete();  // Avisa que a resposta foi dada
+    onRequestClose();  // Fecha o modal após o envio
   };
 
   return (
     <Modal
       isOpen={isOpen}
-      onRequestClose={closeModal}  // Agora chamando a função de fechamento manual
+      onRequestClose={onRequestClose}  // Esta função vai fechar o modal
       className={styles.modalContent}
       overlayClassName={styles.modalOverlay}
-      ariaHideApp={false}
+      ariaHideApp={false}  // Remove o bloqueio de acessibilidade (útil em dev)
     >
       <div className={styles.modalHeader}>
         <h2 className={styles.modalTitle}>Enigma da Ilha {index + 1}</h2>
-        <button 
-          className={styles.closeButton} 
-          ref={closeButtonRef}  // Associando o botão de fechar com a ref
-          onClick={closeModal}  // Fechar o modal ao clicar no botão
-        >
-          &times;
-        </button>
+       
+        
       </div>
-      
+
       <form onSubmit={handleAnswerSubmit}>
         <label className={styles.modalLabel}>
           Responda:
